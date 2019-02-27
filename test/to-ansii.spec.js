6aa315382ade899f019ae64363c8af242979a0fd
Patrick Mooney <pm2@live.co.uk>
2019-02-27 16:32:33 +0000

    properly proportion produced image

--- test/to-ansii.spec.js
+++ test/to-ansii.spec.js
@@ -11,4 +11,13 @@ describe('UNIT - toAnsi', function () {
         const ansii = await terminalArt.toAnsii('./test/sample.png');
         expect(ansii).to.equal(expected);
     });
+
+    it('Should appropriately limit the size of an image', async function () {
+        const imageWidth = 1000;
+        const columns = process.stdout.columns * 0.75;
+        const expectedWidth = Math.ceil(imageWidth / Math.ceil(imageWidth / columns));
+        const ansii = await terminalArt.toAnsii('./test/long.png');
+        const filling = ansii.split("\n")[0].match(/\s/g);
+        expect(filling.length).to.equal(expectedWidth);
+    });
 });
